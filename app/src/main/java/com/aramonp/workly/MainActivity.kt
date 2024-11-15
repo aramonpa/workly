8ebab91fcfe9a83ecabf17c4088211db0d2c9b38
SOBREMESA\Alejandro <alejandro.ramon.palarea@gmail.com>
2024-11-15 19:46:42 +0100

    Minor changes in Home screen Created bottom navigation bar. Created functionality of bottom bar

--- app/src/main/java/com/aramonp/workly/MainActivity.kt
+++ app/src/main/java/com/aramonp/workly/MainActivity.kt
@@ -4,23 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.produceState
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.aramonp.workly.domain.repository.AuthRepository
 import com.aramonp.workly.navigation.NavGraph
 import com.aramonp.workly.navigation.Route
 import com.aramonp.workly.ui.theme.WorklyTheme
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.migration.CustomInjection.inject
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var navHostController: NavHostController
+    private lateinit var navController: NavHostController
 
     @Inject
     lateinit var firebaseAuth: FirebaseAuth
@@ -29,9 +24,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            navHostController = rememberNavController()
+            navController = rememberNavController()
             WorklyTheme {
-                NavGraph(navHostController, startDestination = getStartDestination())
+                NavGraph(navController, startDestination = getStartDestination())
             }
         }
     }
--- app/src/main/java/com/aramonp/workly/navigation/Route.kt
+++ app/src/main/java/com/aramonp/workly/navigation/Route.kt
@@ -5,4 +5,5 @@ sealed class Route (val route: String) {
     data object SignUpScreen : Route(route = "signUpScreen")
     data object HomeScreen : Route(route = "homeScreen")
     data object CalendarScreen : Route(route = "calendarScreen/{id}")
+    data object Settings : Route(route = "settingsScreen")
 }
